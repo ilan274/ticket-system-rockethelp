2f542389b40f1a6acdafa4bbc8cb4dd74c98c4ad
Ilan Herbach <ilan.herbach@gmail.com>
2022-07-24 18:50:18 -0300

    feat: setting onChangeText

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,18 @@
 import { Heading, Icon, VStack, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
+import { useState } from 'react';
 import Logo from '../assets/logo_primary.svg';
 import { Button } from '../components/Button';
 import { Input } from '../components/Input';
 
 export const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { colors } = useTheme();
+
+  const handleSignIn = () => {
+    console.log(email, password);
+  };
 
   return (
     <VStack flex={1} alignItems='center' bg={'gray.600'} px={8} pt={24}>
@@ -19,13 +26,15 @@ export const SignIn = () => {
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
         placeholder='Email'
+        onChangeText={setEmail}
       />
       <Input
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         placeholder='Senha'
         secureTextEntry
+        onChangeText={setPassword}
       />
-      <Button title='Entrar' w='full' mt={8} size='xs' />
+      <Button onPress={handleSignIn} title='Entrar' w='full' mt={8} size='xs' />
     </VStack>
   );
 };
